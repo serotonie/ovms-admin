b4fad1b5c9409e7e59b9898e134541f476ed3538
serotonie <68992449+serotonie@users.noreply.github.com>
2026-08-05 13:21:16 +0200

    Refactor la configuration de la base de données pour améliorer la lisibilité et la gestion des variables d'environnement

--- python/common/src/database/config.py
+++ python/common/src/database/config.py
@@ -5,18 +5,17 @@ import os
 import valkey as redis
 from playhouse.db_url import connect
 
-DB = connect(''.join((
-    'mysql' if (os.getenv('DB_CONNECTION', 'mariadb') == 'mariadb') else os.getenv('DB_CONNECTION'),
-    '://',
-    os.getenv('DB_USERNAME'),
-    ':',
-    os.getenv('DB_PASSWORD'),
-    '@',
-    os.getenv('DB_HOST', 'mariadb'),
-    ':',
-    os.getenv('DB_PORT', '3306'),
-    '/',
-    os.getenv('DB_DATABASE', 'ovms-admin'))))
+DB_CONNECTION = os.getenv('DB_CONNECTION') or 'mariadb'
+DB_DRIVER = 'mysql' if DB_CONNECTION == 'mariadb' else DB_CONNECTION
+DB_USERNAME = os.getenv('DB_USERNAME') or ''
+DB_PASSWORD = os.getenv('DB_PASSWORD') or ''
+DB_HOST = os.getenv('DB_HOST') or 'mariadb'
+DB_PORT = os.getenv('DB_PORT') or '3306'
+DB_DATABASE = os.getenv('DB_DATABASE') or 'ovms-admin'
+
+DB = connect(
+    f'{DB_DRIVER}://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}'
+)
 
 REDIS = redis.Redis(
         os.getenv('REDIS_HOST', 'redis'),
