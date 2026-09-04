39d37deebbbc62ea29ecc70126f86d4c9afe34f6
serotonie <68992449+serotonie@users.noreply.github.com>
2026-09-04 14:34:19 +0000

    feat: add support for X-Forwarded-Port in Nginx configuration and trust local proxy IP

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->trustProxies(
             at: [
+                '127.0.0.1',
                 '10.0.0.0/8',
                 '172.16.0.0/12',
                 '192.168.0.0/16',
